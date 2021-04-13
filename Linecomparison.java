61508f75ae920f286e9b23335b1c8f9310e7630e
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-13 18:07:58 +0530

    ADD:Compare two line

--- Linecomparison.java
+++ Linecomparison.java
@@ -21,15 +21,26 @@ y2=scan.nextDouble();
 result2=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 System.out.println("Length of second line:"+result2);
 
-if(result == result2)
+Double First, Second;
+
+First = new Double (result);
+Second = new Double (result2);
+int res = First.compareTo(Second);
+if(res > 0)
 {
-System.out.println("Line are Equal");
+System.out.println("Line1 is greater than Line2");
+}
+else if(res < 0)
+{
+System.out.println("Line1 is less than Line2");
 }
 else
 {
-System.out.println("Line are not Equal");
+System.out.println("Line1 is equal to Line2");
 }
 }
 }
+
+
 
 
