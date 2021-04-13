8b93ee20eaac8a32354fccfa5d648ad8041c53c7
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-13 17:56:16 +0530

    ADD:Check line equal or not

--- Linecomparison.java
+++ Linecomparison.java
@@ -1,20 +1,35 @@
-import java.util.*;
-
+import java.util.Scanner;
 class Linecomparison
 {
 public static void main(String args[])
 {
-double x1,x2,y1,y2,result;
+double x1,x2,y1,y2,result,result2;
 Scanner scan=new Scanner(System.in);
-System.out.println("Enter the co-ordinates value");
-
+System.out.println("Enter the first co-ordinates value:");
 x1=scan.nextDouble();
 x2=scan.nextDouble();
 y1=scan.nextDouble();
 y2=scan.nextDouble();
-
 result=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-System.out.println("Length of line:"+result);
+System.out.println("Length of first line:"+result);
+
+System.out.println("Enter the Second co-ordinates value:");
+x1=scan.nextDouble();
+x2=scan.nextDouble();
+y1=scan.nextDouble();
+y2=scan.nextDouble();
+result2=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+System.out.println("Length of second line:"+result2);
+
+if(result == result2)
+{
+System.out.println("Line are Equal");
+}
+else
+{
+System.out.println("Line are not Equal");
 }
 }
+}
+
 
